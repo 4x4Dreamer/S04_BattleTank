d4fd67a34b885ea2f8b775446648eecb14eaa6d0
Mike McCutchan <mike@memtrade.com>
2017-08-26 01:16:25 -0400

    Lecture 159 Completing Manual Tank Movement

--- Source/S04_BattleTank/Private/TankMovementComponent.cpp
+++ Source/S04_BattleTank/Private/TankMovementComponent.cpp
@@ -1,7 +1,36 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
 
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 
+	// TODO prevent double speed due to dual control use
+
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//UE_LOG(LogTemp, Warning, TEXT("Intend turn right throw: %f"), Throw);
+
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+
+	// TODO prevent double speed due to dual control use
+
+}
